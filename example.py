3d5132a0c7e4e90ec09349cf8aa7d9ac3d1139b3
andreaxwa <andreaxwa@gmail.com>
2023-09-02 03:34:48 +0000

    Add file example.py with function git_operation.

---
+++ example.py
@@ -0,0 +1,37 @@
+ged, 3 insertions(+), 1 deletion(-)
+student-03-42924cc6d7c6@linux-instance:~/Coursera$ git push
+Warning: Permanently added the ECDSA host key for IP address '140.82.114.3' to the list of known hosts.
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Writing objects: 100% (3/3), 258 bytes | 258.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+To github.com:andreaxwa/Coursera.git
+   0935984..e28409e  main -> main
+student-03-42924cc6d7c6@linux-instance:~/Coursera$ nano example.py
+  GNU nano 5.4                       example.py
+
+
+
+
+
+
+ged, 3 insertions(+), 1 deletion(-)
+student-03-42924cc6d7c6@linux-instance:~/Coursera$ git push
+Warning: Permanently added the ECDSA host key for IP address '140.82.114.3' to the list of known hosts.
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Writing objects: 100% (3/3), 258 bytes | 258.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+To github.com:andreaxwa/Coursera.git
+   0935984..e28409e  main -> main
+student-03-42924cc6d7c6@linux-instance:~/Coursera$ nano example.py
+  GNU nano 5.4                       example.py
+
+def git_operation():
+    print("I am adding example.py file to the remote repository.")
+git_operation()
+
+
+
+
+
